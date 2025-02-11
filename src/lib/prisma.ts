7c210e759711188222258c0f32bc6f78e901f333
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2025-02-11 09:09:52 -0800

    Fixed prisma and changed homepage

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      },
+    },
+    log: ['query', 'error', 'warn']
+  });
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma; 
